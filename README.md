0b27c2c69e1f335b5865ac8b829e92fce541f44e
kandashi <jacobmpalmer@live.co.uk>
2021-06-29 02:25:11 +0100

    first

--- README.md
+++ README.md
@@ -1,71 +1,31 @@
 ![](https://img.shields.io/badge/Foundry-v0.8.6-informational)
-<!--- Downloads @ Latest Badge -->
-<!--- replace <user>/<repo> with your username/repository -->
-<!--- ![Latest Release Download Count](https://img.shields.io/github/downloads/<user>/<repo>/latest/module.zip) -->
+![Latest Release Download Count](https://img.shields.io/github/downloads/kandashi/randomizer-npc/repo>/latest/module.zip)
+![Forge Installs](https://img.shields.io/badge/dynamic/json?label=Forge%20Installs&query=package.installs&suffix=%25&url=https%3A%2F%2Fforge-vtt.com%2Fapi%2Fbazaar%2Fpackage%2Frandomizer-npc&colorB=4aa94a)
 
-<!--- Forge Bazaar Install % Badge -->
-<!--- replace <your-module-name> with the `name` in your manifest -->
-<!--- ![Forge Installs](https://img.shields.io/badge/dynamic/json?label=Forge%20Installs&query=package.installs&suffix=%25&url=https%3A%2F%2Fforge-vtt.com%2Fapi%2Fbazaar%2Fpackage%2F<your-module-name>&colorB=4aa94a) -->
+# Randomizer NPC
 
+This allows for your unlinked tokens to be that little extra bit different. Randomize available weapons, spells, hp and ability scores; all by just dropping a token onto the canvas.
 
-# How to use this Template to create a versioned Release
+## HP
 
-1. Open your repository's releases page.
+Auto rolls for a tokens HP (does not send chat message)
 
-![Where to click to open repository releases.](https://user-images.githubusercontent.com/7644614/93409301-9fd25080-f864-11ea-9e0c-bdd09e4418e4.png)
+## Ability Scores
 
-2. Click "Draft a new release"
+Rolls a user defined formula to apply to each ability score (default of +1d4-2)
 
-![Draft a new release button.](https://user-images.githubusercontent.com/7644614/93409364-c1333c80-f864-11ea-89f1-abfcb18a8d9f.png)
+## Randomize Weapons
 
-3. Fill out the release version as the tag name.
+Reduces the weapons on the Actor down to a specified amount by either melee/ranged weapons
+Configure this level within the Special Traits menu of the *Original Actor* 
 
-If you want to add details at this stage you can, or you can always come back later and edit them.
+## Randomize Spells
 
-![Release Creation Form](https://user-images.githubusercontent.com/7644614/93409543-225b1000-f865-11ea-9a19-f1906a724421.png)
+Reduces the spells on the actor down to a specified amount by spell level.
+Configure this level within the Special Traits menu of the *Original Actor*. The value should be numbers split by `;` increasing in spell level from cantrip to 9th level
+`4;3;2;2;1` would result in 4 cantrips, 3 first level, 2 second and third level and 1 fourth level on the unlinked token.
 
-4. Hit submit.
+## Randomize Race
 
-5. Wait a few minutes.
-
-A Github Action will run to populate the `module.json` and `module.zip` with the correct urls that you can then use to distribute this release. You can check on its status in the "Actions" tab.
-
-![Actions Tab](https://user-images.githubusercontent.com/7644614/93409820-c1800780-f865-11ea-8c6b-c3792e35e0c8.png)
-
-6. Grab the module.json url from the release's details page.
-
-![image](https://user-images.githubusercontent.com/7644614/93409960-10c63800-f866-11ea-83f6-270cc5d10b71.png)
-
-This `module.json` will only ever point at this release's `module.zip`, making it useful for sharing a specific version for compatibility purposes.
-
-7. You can use the url `https://github.com/<user>/<repo>/releases/latest/download/module.json` to refer to the manifest.
-
-This is the url you want to use to install the module typically, as it will get updated automatically.
-
-# How to List Your Releases on Package Admin
-
-To request a package listing for your first release, go to the [Package Submission Form](https://foundryvtt.com/packages/submit) (accessible via a link at the bottom of the "[Systems and Modules](https://foundryvtt.com/packages/)" page on the Foundry website).
-
-Fill in the form. "Package Name" must match the name in the module manifest.  Package Title will be the display name for the package.  Package URL should be your repo URL.
-![image](https://user-images.githubusercontent.com/36359784/120664263-b49e5500-c482-11eb-9126-af7006389903.png)
-
-
-One of the Foundry staff will typically get back to you with an approval or any further questions within a few days, and give you access to the package admin pages.
-
-Once you have access to the [module admin page](https://foundryvtt.com/admin/packages/package/), you can release a new version by going into the page for your module, scrolling to the bottom, and filling in a new Package Version.
-
-When listing a new version, Version should be the version number you set above, and the Manifest URL should be the manifest __for that specific version__ (do not use /latest/ here).
-![image](https://user-images.githubusercontent.com/36359784/120664346-c4b63480-c482-11eb-9d8b-731b50d70939.png)
-
-> ### :warning: Important :warning:
-> 
-> It is very important that you use the specific release manifest url, and not the `/latest` url here. For more details about why this is important and how Foundry Installs/Updates packages, read [this wiki article](https://foundryvtt.wiki/en/development/guides/releases-and-history).
-
-Clicking "Save" in the bottom right will save the new version, which means that anyone installing your module from within Foundry will get that version, and a post will be generated in the #release-announcements channel on the official Foundry VTT Discord.
-
-
-# FoundryVTT Module
-
-Does something, probably
-
-## Changelog
+Adds a item from a compendium of `races` to determine token race. The bundled compendium comes with Active Effects for the race traits, I suggest reducing the randomization slightly to offset these bonuses (1d3-2 perhaps)
+Configure this within the Special Traits menu of the *Original Actor*
---
+++ scripts/Randomizer.js
@@ -0,0 +1,204 @@
+Hooks.on('init', () => {
+
+    /** name of the table on which to roll if a surge occurs */
+    game.settings.register("randomizer-npc", "namelist", {
+        name: "Randomizer names",
+        hint: "Names of actors to randomize, seperate with ; ",
+        scope: "world",
+        config: true,
+        default: "",
+        type: String,
+    });
+
+    game.settings.register("randomizer-npc", "hpRoll", {
+        name: "Randomizer HP",
+        hint: "Auto roll HP for new tokens",
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean,
+    })
+
+    game.settings.register("randomizer-npc", "randomizeWeapons", {
+        name: "Randomizer Weapons",
+        hint: "Auto reduce tokens weapons to specified values",
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean,
+    })
+
+    game.settings.register("randomizer-npc", "randomizeRace", {
+        name: "Randomizer Race",
+        hint: "Auto apply Racial Feature from compendium",
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean,
+    })
+
+    game.settings.register("randomizer-npc", "raceCompendium", {
+        name: "Race Pack name",
+        hint: "Name of the compendium to draw race items from",
+        scope: "world",
+        config: true,
+        default: "",
+        type: String,
+    })
+
+    game.settings.register("randomizer-npc", "crRangeEnable", {
+        name: 'Enable CR randomization',
+        hint: 'Enables randomization for any create of CR equal or lower than the set value',
+        scope: 'world',
+        type: Boolean,
+        default: false,
+        config: true,
+    });
+
+    game.settings.register('randomizer-npc', 'crRange', {
+        name: 'CR range for randomization',
+        hint: 'What level of CR creature should be randomized, numeric. Use 0.25 for 1/4',
+        scope: 'world',
+        config: true,
+        default: 0,
+        type: Number,
+    });
+
+    game.settings.register("randomizer-npc", "randomizer", {
+        name: "Ability Randomizer Value",
+        hint: "Calculation of randomization. This is the calculation applied to every stat of a randomized creature",
+        scope: "world",
+        config: true,
+        default: "+1d4-2",
+        type: String,
+    });
+
+})
+
+Hooks.on("init", () => {
+    CONFIG.DND5E.characterFlags.randomizerMelee = {
+        hint: "Number of melee weapons to keep on randomization",
+        name: "NPC melee weapons",
+        section: "Randomization",
+        type: Number
+    }
+    CONFIG.DND5E.characterFlags.randomizerRanged = {
+        hint: "Number of ranged weapons to keep on randomization",
+        name: "NPC ranged weapons",
+        section: "Randomization",
+        type: Number
+    }
+    CONFIG.DND5E.characterFlags.randomizerSpellProg = {
+        hint: "Number of spells of each level should be randomized, split by ';'",
+        name: "NPC Spells per Level",
+        section: "Randomization",
+        type: String
+    }
+    CONFIG.DND5E.characterFlags.randomizerRace = {
+        hint: "Randomly assign a race item",
+        name: "Randomize Race",
+        section: "Randomization",
+        type: Boolean
+    }
+
+})
+
+function Randomize(randomization, token) {
+    let newData = {
+        "data": {
+            "abilities": {
+                "cha": {
+                    "value": token.actor.data.data.abilities.cha.value + new Roll(randomization).roll().total
+                },
+                "con": {
+                    "value": token.actor.data.data.abilities.con.value + new Roll(randomization).roll().total
+                },
+                "dex": {
+                    "value": token.actor.data.data.abilities.dex.value + new Roll(randomization).roll().total
+                },
+                "int": {
+                    "value": token.actor.data.data.abilities.int.value + new Roll(randomization).roll().total
+                },
+                "str": {
+                    "value": token.actor.data.data.abilities.str.value + new Roll(randomization).roll().total
+                },
+                "wis": {
+                    "value": token.actor.data.data.abilities.wis.value + new Roll(randomization).roll().total
+                }
+            }
+        }
+
+    }
+    token.update({ "actorData": newData })
+}
+
+Hooks.on("createToken", async (tokenData, options, id) => {
+    if(tokenData.isLinked) return;
+    let token = canvas.tokens.get(tokenData._id);
+    let nameList = game.settings.get("randomizer-npc", "namelist").split(";");
+    let randomization = game.settings.get('randomizer-npc', 'randomizer');
+
+    if ((game.settings.get('randomizer-npc', 'crRangeEnable') == true)) {
+        let crRange = game.settings.get('randomizer-npc', 'crRange')
+        if (token.actor.data.data.details.cr <= crRange) {
+            Randomize(randomization, token)
+        }
+    }
+    else if (nameList.includes(tokenData.name)) {
+        Randomize(randomization, token)
+    }
+
+    if (game.settings.get("randomizer-npc", "hpRoll")) {
+        const formula = token.actor.data.data.attributes.hp.formula;
+        if (!formula) return;
+        const hp = new Roll(formula).roll().total;
+        AudioHelper.play({ src: CONFIG.sounds.dice });
+        await token.actor.update({ "data.attributes.hp.value": hp, "data.attributes.hp.max": hp });
+    }
+
+    if (game.settings.get("randomizer-npc", "randomizeWeapons")) {
+        let { randomizerMelee, randomizerRanged, randomizerSpellProg } = token.actor.data.flags.dnd5e
+        let meleeWeapons = token.actor.items.filter(i => i.data.data.actionType === "mwak")
+        let rangedWeapons = token.actor.items.filter(i => i.data.data.actionType === "rwak")
+        let spells = token.actor.items.filter(i => i.data.type === "spell")
+        let deletedMelee = !!randomizerMelee ? await restrictWeapons(meleeWeapons, randomizerMelee) : []
+        let deletedRanged = !!randomizerRanged ? await restrictWeapons(rangedWeapons, randomizerRanged) : []
+        let deletedSpells = !!randomizerSpellProg ? await restrictSpells(spells, randomizerSpellProg) : []
+        let update = deletedMelee.concat(deletedRanged, deletedSpells)
+        if (update.length > 0) {
+            await token.actor.deleteEmbeddedDocuments("Item", update)
+        }
+    }
+
+    if(game.settings.get("randomizer-npc", "randomizeRace")){
+        let userPackName = game.settings.get("randomizer-npc", "raceCompendium")
+        let pack = !!userPackName ? game.packs.find( i => i.metadata.name === userPackName) : game.packs.find( i => i.metadata.name === "Randomizer Races")
+        let index = Math.floor(Math.random() * pack.index.size | 0)
+        let race = await pack.getDocument(pack.index.contents[index]._id)
+        await token.actor.createEmbeddedDocuments("Item", [race.data])
+    }
+
+})
+
+async function restrictSpells(spells, prog) {
+    prog = prog.split(";")
+    // "3,2,1,0,0,0,0,0"
+    let spellArray = []
+    for (let l = 0; l < 10; l++) {
+        let leveled = spells.filter(i => i.data.data.level === l)
+        let filtered = await restrictWeapons(leveled, prog[l])
+        spellArray = spellArray.concat(filtered)
+    }
+    return spellArray
+}
+
+async function restrictWeapons(weapons, value) {
+    if (typeof value === "string") value = parseInt(value)
+    let ids = []
+    for (var i = 1; i <= weapons.length - value; i) {
+        let index = Math.floor(Math.random() * weapons.length | 0)
+        ids.push(weapons[index].id)
+        weapons.splice(index, 1)
+    }
+    return ids
+}
